a84d7e8fc8c234be7d3da1de66254e8ed22ebe41
Pavol Rusnak <stick@gk2.sk>
2018-10-11 00:59:18 +0200

    style: run black, isort and autoflake

--- messages.py
+++ messages.py
@@ -1,7 +1,7 @@
 import binascii
 import string
-import requests
 
+import requests
 
 messages = {}
 
@@ -11,36 +11,36 @@ def process_msg(msg):
     try:
 
         # single message
-        if 'concat' not in msg:
+        if "concat" not in msg:
             # push it
-            pushtx(msg['text'])
+            pushtx(msg["text"])
             return
 
         # split message
-        ref = '%s:%s' % (msg['msisdn'], msg['concat-ref'])
-        idx = int(msg['concat-part'])
-        cnt = int(msg['concat-total'])
+        ref = "%s:%s" % (msg["msisdn"], msg["concat-ref"])
+        idx = int(msg["concat-part"])
+        cnt = int(msg["concat-total"])
 
         # not seen before
         if ref not in messages:
             messages[ref] = cnt * [None]
 
         # assign value
-        messages[ref][idx - 1] = msg['text']
+        messages[ref][idx - 1] = msg["text"]
 
         # are all value set?
         if None not in messages[ref]:
             # get joined message
-            joined = ''.join(messages[ref])
+            joined = "".join(messages[ref])
             del messages[ref]
             # and push it
             pushtx(joined)
 
-        print('PROCESSED OK')
+        print("PROCESSED OK")
 
     except Exception as ex:
 
-        print('PROCESS ERROR', ex)
+        print("PROCESS ERROR", ex)
 
 
 def pushtx(data):
@@ -58,22 +58,22 @@ def pushtx(data):
             decoded = None
 
     if not decoded:
-        print('PUSH DECODE ERROR')
+        print("PUSH DECODE ERROR")
         return
 
     endpoints = [
-        'https://btc-bitcore1.trezor.io/api/tx/send',
-        'https://btc-bitcore4.trezor.io/api/tx/send',
-        'https://insight.bitpay.com/api/tx/send',
-        'https://blockexplorer.com/api/tx/send',
+        "https://btc-bitcore4.trezor.io/api/tx/send",
+        "https://btc-bitcore5.trezor.io/api/tx/send",
+        "https://insight.bitpay.com/api/tx/send",
+        "https://blockexplorer.com/api/tx/send",
     ]
 
     for e in endpoints:
         try:
-            r = requests.post(e, json={'rawtx': decoded}, timeout=1)
+            r = requests.post(e, json={"rawtx": decoded}, timeout=1)
             if r.status_code == 200:
-                print('PUSH OK', e)
+                print("PUSH OK", e)
             else:
-                print('PUSH ERROR', r.status_code, e)
+                print("PUSH ERROR", r.status_code, e)
         except Exception as ex:
-            print('PUSH FAILED', e, ex)
+            print("PUSH FAILED", e, ex)
--- smspushtx.py
+++ smspushtx.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python3
 
 from flask import Flask, request
-
 from messages import process_msg
 
 app = Flask(__name__)
 
 
-@app.route('/smspushtx', methods=['POST'])
+@app.route("/smspushtx", methods=["POST"])
 def smspushtx():
     j = request.get_json()
     print(j)
     process_msg(j)
-    return '', 200
+    return "", 200
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=13730)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=13730)
