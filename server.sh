4b8ac9509eb17d7267f5a169ee9450c43e2ca050
Pavol Rusnak <pavol@rusnak.io>
2019-03-23 22:58:58 +0100

    use gunicorn

---
+++ server.sh
@@ -0,0 +1,1 @@
+gunicorn smspushtx:app -b 0.0.0.0:13730
--- smspushtx.py
+++ smspushtx.py
@@ -12,7 +12,3 @@ def smspushtx():
     print(j)
     process_msg(j)
     return "", 200
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=13730)
